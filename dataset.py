9213a9b6546d291cdc141818acd631b0f8881aba
pritansh <pritansh.chaudhary@gmail.com>
2017-02-18 14:19:50 +0530

    Class Structure

---
+++ dataset.py
@@ -0,0 +1,35 @@
+import os
+import numpy as np
+
+class Dataset:
+
+    def __init__(self, directory_path='', train_perc=0.8):
+        self.class_dirs = os.listdir(directory_path)
+        classes = len(self.class_dirs)
+        self.train_files = np.empty(classes, dtype='object')
+        self.test_files = np.empty(classes, dtype='object')
+        for i in range(0, classes):
+            dir_path = directory_path + '/' + self.class_dirs[i]
+            files = np.core.defchararray.add(dir_path + '/', os.listdir(dir_path))
+            np.random.shuffle(files)
+            train_size = int(len(files) * train_perc)
+            self.train_files[i] = np.array(files[:train_size])
+            self.test_files[i] = np.array(files[train_size:])
+
+    def __str__(self):
+        data_str = ''
+        for i in range(0, len(self.class_dirs)):
+            data_str += str(i+1) + '->' + self.class_dirs[i] + ' (' + str(len(self.train_files[i]))
+            data_str += ',' + str(len(self.test_files[i])) + ')'
+            if i+1 < len(self.class_dirs):
+                data_str += '\n'
+        return data_str
+
+    def __repr__(self):
+        data_str = ''
+        for i in range(0, len(self.class_dirs)):
+            data_str += str(i+1) + '->' + self.class_dirs[i] + ' (' + str(len(self.train_files[i]))
+            data_str += ',' + str(len(self.test_files[i])) + ')'
+            if i+1 < len(self.class_dirs):
+                data_str += '\n'
+        return data_str
